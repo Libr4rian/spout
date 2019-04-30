fda0afbecc68aeedaf9e4bf3b49d5e17d6996ccb
Florian Federighi <florian.federighi@hcube-conseil.fr>
2019-04-30 13:13:53 +0200

    Add column width

--- src/Spout/Writer/XLSX/Writer.php
+++ src/Spout/Writer/XLSX/Writer.php
@@ -30,6 +30,26 @@ class Writer extends AbstractMultiSheetsWriter
     /** @var Internal\Workbook The workbook for the XLSX file */
     protected $book;
 
+    /** @var array Collection of column dimensions */
+    protected $columnsWidth = [];
+
+    /**
+     * Defines column width for one or more columns of the worksheet.
+     *
+     * @param array $widths
+     * @return Writer
+     */
+    public function setColumnsWidth($widths)
+    {
+        $this->columnsWidth = $widths;
+
+        if ($this->book) {
+            $this->book->setColumnsWidth($this->columnsWidth);
+        }
+
+        return $this;
+    }
+
     /**
      * Sets a custom temporary folder for creating intermediate files/folders.
      * This must be set before opening the writer.
@@ -74,7 +94,7 @@ class Writer extends AbstractMultiSheetsWriter
     {
         if (!$this->book) {
             $tempFolder = ($this->tempFolder) ? : sys_get_temp_dir();
-            $this->book = new Workbook($tempFolder, $this->shouldUseInlineStrings, $this->shouldCreateNewSheetsAutomatically, $this->defaultRowStyle);
+            $this->book = new Workbook($tempFolder, $this->shouldUseInlineStrings, $this->shouldCreateNewSheetsAutomatically, $this->defaultRowStyle, $this->columnsWidth);
             $this->book->addNewSheetAndMakeItCurrent();
         }
     }
